1e98d93c7fa7dd568c46049509849dd2b7912513
zhang wei <330416172@qq.com>
2019-05-21 18:54:58 +0800

    edit

---
+++ sirius-transport/sirius-transport-api/src/main/java/org/sirius/transport/api/exception/IoSignals.java
@@ -0,0 +1,16 @@
+
+package org.sirius.transport.api.exception;
+
+import org.sirius.common.util.Signal;
+
+public class IoSignals {
+
+    /** 错误的MAGIC */
+    public static final Signal ILLEGAL_MAGIC    = Signal.valueOf(IoSignals.class, "ILLEGAL_MAGIC");
+    /** 错误的消息标志位 */
+    public static final Signal ILLEGAL_SIGN     = Signal.valueOf(IoSignals.class, "ILLEGAL_SIGN");
+    /** Read idle 链路检测 */
+    public static final Signal READER_IDLE      = Signal.valueOf(IoSignals.class, "READER_IDLE");
+    /** Protocol body 太大 */
+    public static final Signal BODY_TOO_LARGE   = Signal.valueOf(IoSignals.class, "BODY_TOO_LARGE");
+}
--- sirius-transport/sirius-transport-api/src/main/java/org/sirius/transport/api/payload/Response.java
+++
@@ -1,9 +0,0 @@
-package org.sirius.transport.api.payload;
-
-public class Response extends MessagePayload{
-
-	public Response(long id) {
-		super(id);
-	}
-
-}
