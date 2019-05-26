bed75921050efa8ce9428ac9b3b6e33c618bf65d
zhang wei <330416172@qq.com>
2019-05-26 09:08:30 +0800

    eidt

--- sirius-transport/sirius-transport-netty/src/main/java/org/sirius/transport/netty/handler/connector/ConnectorHandler.java
+++ sirius-transport/sirius-transport-netty/src/main/java/org/sirius/transport/netty/handler/connector/ConnectorHandler.java
@@ -11,6 +11,7 @@ import org.sirius.transport.api.Response;
 import org.sirius.transport.netty.channel.NettyChannel;
 
 import io.netty.channel.Channel;
+import io.netty.channel.ChannelConfig;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
@@ -32,35 +33,66 @@ public class ConnectorHandler extends ChannelInboundHandlerAdapter {
 
 	@Override
 	public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
-		if(msg instanceof Response) {
+		if (msg instanceof Response) {
 			try {
 				processor.handleResponse(NettyChannel.attachChannel(ctx.channel()), (Response) msg);
-			}catch(Throwable t) {
-			   logger.error("An exception was caught: {}, on {} #channelRead().", StackTraceUtil.stackTrace(t),  ctx.channel());
+			} catch (Throwable t) {
+				logger.error("An exception was caught: {}, on {} #channelRead().", StackTraceUtil.stackTrace(t),
+						ctx.channel());
 			}
-			
-		}else {
+
+		} else {
 			logger.warn("Unexpected message type received: {}, channel: {}.", msg.getClass(), ctx.channel());
 		}
 	}
 
 	@Override
+	public void channelWritabilityChanged(ChannelHandlerContext ctx) throws Exception {
+		Channel ch = ctx.channel();
+		ChannelConfig config = ch.config();
+
+		// 高水位线: ChannelOption.WRITE_BUFFER_HIGH_WATER_MARK
+		// 低水位线: ChannelOption.WRITE_BUFFER_LOW_WATER_MARK
+		if (!ch.isWritable()) {
+			// 当前channel的缓冲区(OutboundBuffer)大小超过了WRITE_BUFFER_HIGH_WATER_MARK
+			if (logger.isWarnEnabled()) {
+				logger.warn(
+						"{} is not writable, high water mask: {}, the number of flushed entries that are not written yet: {}.",
+						ch, config.getWriteBufferHighWaterMark(), ch.unsafe().outboundBuffer().size());
+			}
+
+			config.setAutoRead(false);
+		} else {
+			// 曾经高于高水位线的OutboundBuffer现在已经低于WRITE_BUFFER_LOW_WATER_MARK了
+			if (logger.isWarnEnabled()) {
+				logger.warn(
+						"{} is writable(rehabilitate), low water mask: {}, the number of flushed entries that are not written yet: {}.",
+						ch, config.getWriteBufferLowWaterMark(), ch.unsafe().outboundBuffer().size());
+			}
+
+			config.setAutoRead(true);
+		}
+	}
+
+	@Override
 	public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) throws Exception {
-           Channel ch = ctx.channel();
-           if(cause instanceof Signal) {
-        	   logger.error("I/O signal was caught: {}, force to close channel: {}.", ((Signal) cause).name(), ch);
-              
-        	   ch.close();
-           }else if(cause instanceof IOException){
-        	   logger.error("I/O exception was caught: {}, force to close channel: {}.", StackTraceUtil.stackTrace(cause), ch);
-              
-        	   ch.close();
-           }else if(cause instanceof DecoderException){
-        	   logger.error("Decoder exception was caught: {}, force to close channel: {}.", StackTraceUtil.stackTrace(cause), ch);
-               
-        	   ch.close();
-           }else {
-        	   logger.error("Unexpected exception was caught: {}, channel: {}.", StackTraceUtil.stackTrace(cause), ch);
-           }
+		Channel ch = ctx.channel();
+		if (cause instanceof Signal) {
+			logger.error("I/O signal was caught: {}, force to close channel: {}.", ((Signal) cause).name(), ch);
+
+			ch.close();
+		} else if (cause instanceof IOException) {
+			logger.error("I/O exception was caught: {}, force to close channel: {}.", StackTraceUtil.stackTrace(cause),
+					ch);
+
+			ch.close();
+		} else if (cause instanceof DecoderException) {
+			logger.error("Decoder exception was caught: {}, force to close channel: {}.",
+					StackTraceUtil.stackTrace(cause), ch);
+
+			ch.close();
+		} else {
+			logger.error("Unexpected exception was caught: {}, channel: {}.", StackTraceUtil.stackTrace(cause), ch);
+		}
 	}
 }
