5d56796aa83efd420efb8694d1618a516d2034c9
zhang wei <330416172@qq.com>
2019-05-29 18:32:47 +0800

    edit

--- sirius-rpc/src/main/java/org/sirius/rpc/executor/disruptor/DisruptorExecutor.java
+++ sirius-rpc/src/main/java/org/sirius/rpc/executor/disruptor/DisruptorExecutor.java
@@ -1,56 +1,157 @@
 package org.sirius.rpc.executor.disruptor;
 
-import org.sirius.rpc.consumer.RequestTask;
+import java.util.concurrent.ThreadFactory;
+import java.util.concurrent.TimeUnit;
+
+import org.sirius.common.concurrent.NamedThreadFactory;
+import org.sirius.common.util.Constants;
+import org.sirius.common.util.Pow2;
+import org.sirius.common.util.Requires;
+import org.sirius.common.util.internal.logging.InternalLogger;
+import org.sirius.common.util.internal.logging.InternalLoggerFactory;
 import org.sirius.rpc.executor.InnerExecutor;
-import org.sirius.transport.api.Request;
 
+import com.lmax.disruptor.BlockingWaitStrategy;
+import com.lmax.disruptor.BusySpinWaitStrategy;
 import com.lmax.disruptor.EventFactory;
+import com.lmax.disruptor.InsufficientCapacityException;
+import com.lmax.disruptor.LiteBlockingWaitStrategy;
+import com.lmax.disruptor.LiteTimeoutBlockingWaitStrategy;
+import com.lmax.disruptor.PhasedBackoffWaitStrategy;
 import com.lmax.disruptor.RingBuffer;
-import com.lmax.disruptor.WorkHandler;
+import com.lmax.disruptor.SleepingWaitStrategy;
+import com.lmax.disruptor.TimeoutBlockingWaitStrategy;
+import com.lmax.disruptor.WaitStrategy;
+import com.lmax.disruptor.YieldingWaitStrategy;
 import com.lmax.disruptor.dsl.Disruptor;
-import com.lmax.disruptor.util.DaemonThreadFactory;
+import com.lmax.disruptor.dsl.ProducerType;
 
-public class DisruptorExecutor implements InnerExecutor{
 
-	private RingBuffer<Event> buffer;
-	private Disruptor<Event> disruptor; 
-	
-	public DisruptorExecutor() {
-		disruptor = new Disruptor<Event>(Event.FACTORY,1024,DaemonThreadFactory.INSTANCE);
-		disruptor.handleEventsWithWorkerPool(new RequestTaskHandler());
-		buffer = disruptor.start();
+/**
+* 可选择的等待策略, 越往下越极端:
+*
+* The default wait strategy used by the Disruptor is the BlockingWaitStrategy.
+*
+* BlockingWaitStrategy:
+* Internally the BlockingWaitStrategy uses a typical lock and condition variable to handle thread wake-up.
+* The BlockingWaitStrategy is the slowest of the available wait strategies,
+* but is the most conservative with the respect to CPU usage and will give the most consistent behaviour across
+* the widest variety of deployment options. However, again knowledge of the deployed system can allow for additional
+* performance.
+*
+* SleepingWaitStrategy:
+* Like the BlockingWaitStrategy the SleepingWaitStrategy it attempts to be conservative with CPU usage,
+* by using a simple busy wait loop, but uses a call to LockSupport.parkNanos(1) in the middle of the loop.
+* On a typical Linux system this will pause the thread for around 60µs.
+* However it has the benefit that the producing thread does not need to take any action other increment the appropriate
+* counter and does not require the cost of signalling a condition variable. However, the mean latency of moving the
+* event between the producer and consumer threads will be higher. It works best in situations where low latency is not
+* required, but a low impact on the producing thread is desired. A common use case is for asynchronous logging.
+*
+* YieldingWaitStrategy:
+* The YieldingWaitStrategy is one of 2 Wait Strategies that can be use in low latency systems,
+* where there is the option to burn CPU cycles with the goal of improving latency.
+* The YieldingWaitStrategy will busy spin waiting for the sequence to increment to the appropriate value.
+* Inside the body of the loop Thread.yield() will be called allowing other queued threads to run.
+* This is the recommended wait strategy when need very high performance and the number of Event Handler threads is
+* less than the total number of logical cores, e.g. you have hyper-threading enabled.
+*
+* BusySpinWaitStrategy:
+* The BusySpinWaitStrategy is the highest performing Wait Strategy, but puts the highest constraints on the deployment
+* environment. This wait strategy should only be used if the number of Event Handler threads is smaller than the number
+* of physical cores on the box. E.g. hyper-threading should be disabled
+*
+*/
+public class DisruptorExecutor implements InnerExecutor {
+
+	private static final InternalLogger logger = InternalLoggerFactory.getInstance(LoggingExceptionHandler.class);
+	private static final EventFactory<MessageEvent<Runnable>> eventFactory = MessageEvent::new;
+	static int BUFFER_SIZE = 32768;
+	int MAX_NUM_WORKERS = Constants.AVAILABLE_PROCESSORS << 3;
+
+	private final Disruptor<MessageEvent<Runnable>> disruptor;
+
+	public DisruptorExecutor(int numWorkers, ThreadFactory threadFactory) {
+	        this(numWorkers, threadFactory, BUFFER_SIZE, WaitStrategyType.BLOCKING_WAIT);
+	    }
+	public DisruptorExecutor(int numWorkers, ThreadFactory threadFactory, int bufSize,
+			WaitStrategyType waitStrategyType) {
+
+		Requires.requireTrue(bufSize > 0, "bufSize must be larger than 0");
+		if (!Pow2.isPowerOfTwo(bufSize)) {
+			bufSize = Pow2.roundToPowerOfTwo(bufSize);
+		}
+		WaitStrategy waitStrategy;
+		switch (waitStrategyType) {
+		case BLOCKING_WAIT:
+			waitStrategy = new BlockingWaitStrategy();
+			break;
+		case LITE_BLOCKING_WAIT:
+			waitStrategy = new LiteBlockingWaitStrategy();
+			break;
+		case TIMEOUT_BLOCKING_WAIT:
+			waitStrategy = new TimeoutBlockingWaitStrategy(1000, TimeUnit.MILLISECONDS);
+			break;
+		case LITE_TIMEOUT_BLOCKING_WAIT:
+			waitStrategy = new LiteTimeoutBlockingWaitStrategy(1000, TimeUnit.MILLISECONDS);
+			break;
+		case PHASED_BACK_OFF_WAIT:
+			waitStrategy = PhasedBackoffWaitStrategy.withLiteLock(1000, 1000, TimeUnit.NANOSECONDS);
+			break;
+		case SLEEPING_WAIT:
+			waitStrategy = new SleepingWaitStrategy();
+			break;
+		case YIELDING_WAIT:
+			waitStrategy = new YieldingWaitStrategy();
+			break;
+		case BUSY_SPIN_WAIT:
+			waitStrategy = new BusySpinWaitStrategy();
+			break;
+		default:
+			throw new UnsupportedOperationException(waitStrategyType.toString());
+		}
+		if (threadFactory == null) {
+			threadFactory = new NamedThreadFactory("disruptor.processor");
+		}
+		Disruptor<MessageEvent<Runnable>> dr = new Disruptor<>(eventFactory, bufSize, threadFactory, ProducerType.MULTI,waitStrategy);
+		dr.setDefaultExceptionHandler(new LoggingExceptionHandler());
+		numWorkers = Math.min(Math.abs(numWorkers), MAX_NUM_WORKERS);
+		if (numWorkers == 1) {
+			dr.handleEventsWith(new TaskHandler());
+		} else {
+			TaskHandler[] handlers = new TaskHandler[numWorkers];
+			for (int i = 0; i < numWorkers; i++) {
+				handlers[i] = new TaskHandler();
+			}
+			dr.handleEventsWithWorkerPool(handlers);
+		}
+		dr.start();
+		disruptor = dr;
 		
 	}
+
 	@Override
-	public void execute(Runnable task) {
-		
-		long next = buffer.next();
-		
-		try {
-			Event event = buffer.get(next);
-			event.task = (RequestTask) task;
-		}finally {
-			buffer.publish(next);
-			System.out.println("序号为"+next);
-		}
-		
+	public void execute(Runnable message) {
+		 RingBuffer<MessageEvent<Runnable>> ringBuffer = disruptor.getRingBuffer();
+	        try {
+	            long sequence = ringBuffer.tryNext();
+	            try {
+	                MessageEvent<Runnable> event = ringBuffer.get(sequence);
+	                event.setMessage(message);
+	            } finally {
+	                ringBuffer.publish(sequence);
+	            }
+	           
+	        } catch (InsufficientCapacityException e) {
+	        	 if (logger.isWarnEnabled()) {
+	                 logger.warn("Disruptor  InsufficientCapacityException ..................");
+	             }
+	        }
+
 	}
 
 	@Override
 	public void close() {
-		// TODO Auto-generated method stub
-		
+		disruptor.shutdown();
 	}
-	 public static void main(String[] args) throws InterruptedException
-	    {
-	       
-		 DisruptorExecutor d = new DisruptorExecutor();
-	        for (int i = 0; i < 1000000; i++)
-	        {
-	        	RequestTask task = new RequestTask(new Request());
-	        	d.execute(task);
-	        }
-	        Thread.sleep(50000);
-	    }
-	
 }
