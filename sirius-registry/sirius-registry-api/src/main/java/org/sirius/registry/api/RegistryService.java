aeb2373c07f289f43da257f90899f58a554d3bf4
zhang wei <330416172@qq.com>
2019-06-03 11:05:46 +0800

    edit

--- sirius-registry/sirius-registry-api/src/main/java/org/sirius/registry/api/RegistryService.java
+++ sirius-registry/sirius-registry-api/src/main/java/org/sirius/registry/api/RegistryService.java
@@ -5,4 +5,8 @@ import org.sirius.config.ProviderConfig;
 public interface RegistryService {
 
 	public void register(ProviderConfig config);
+	
+	public void Unregister(ProviderConfig config);
+	
+	public List<> 
 }
