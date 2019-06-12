429e715eefc50ecd746276ff2cdc4da56a151546
zhang wei <330416172@qq.com>
2019-06-12 21:54:58 +0800

    edit

---
+++ sirius-spring/src/main/java/org/sirius/spring/schema/SiriusNamespaceHandler.java
@@ -0,0 +1,11 @@
+package org.sirius.spring.schema;
+
+import org.springframework.beans.factory.xml.NamespaceHandlerSupport;
+
+public class SiriusNamespaceHandler extends NamespaceHandlerSupport {
+
+	@Override
+	public void init() {
+		
+	}
+}
